482b56fd7109a56a57bd3fba95f570c67c0c14b5
Sebastian Hoof <s.hoof.physics@gmail.com>
2024-12-06 12:45:34 +0100

    Added documentation for model building routines.

--- ksvz_models/model_building.py
+++ ksvz_models/model_building.py
@@ -16,34 +16,129 @@ repinfo = np.genfromtxt(file_path+"/data/rep_info.dat", dtype='int64')
 alphaSinfo = np.load(file_path+"/data/running_alphaS_SM.npy")
 
 def print_replist():
+    """
+    Print the list of representations from the data file.
+    """
     print(repinfo)
     
 @njit
 def get_max_index(d: int = 6) -> int:
+    """
+    Return the maximum index of the representations with dimension d (useful for making a subselection).
+
+    Parameters
+    ----------
+    d : int
+        Dimension of the representations (default: 6)
+
+    Returns
+    -------
+    int
+        Maximum index of the representations with dimension d
+    """
     return np.where(repinfo[:,3] == d)[0][-1]
 
 @njit
 def dynkins(rep_index: int, repinfo: np.ndarray = repinfo) -> np.ndarray[int]:
+    """
+    Return the Dynkin labels of a representation
+
+    Parameters
+    ----------
+    rep_index : int
+        Index of the representation
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    np.ndarray[int]
+        Array of Dynkin labels
+    """
     return repinfo[rep_index][4:7]
 
 @njit
 def casimirs(rep_index: int, repinfo: np.ndarray = repinfo) -> np.ndarray[int]:
+    """
+    Return the Casimir invariants of a representation
+
+    Parameters
+    ----------
+    rep_index : int
+        Index of the representation
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    np.ndarray[int]
+        Array of Casimir invariants
+    """
     return repinfo[rep_index][7:10]
 
 @njit
 def charges_from_rep(rep: int, repinfo: np.ndarray = repinfo) -> tuple[int, ...]:
+    """
+    Return the charges of a representation
+
+    Parameters
+    ----------
+    rep : int
+        Representation index
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    tuple[int, ...]
+        Tuple of the charges of the representation, the index, and the "sign" of the representation
+    """
     ind, sgn = abs(rep) - 1, sign(rep)
     r3, r2, r1 = repinfo[ind][:3]
     return r3, r2, r1, ind, sgn
 
 @njit
 def min_dim_from_rep(rep: int, repinfo: np.ndarray = repinfo) -> int:
+    """
+    Return the minimum dimension any the decay operator that can be associated with a given representation
+
+    Parameters
+    ----------
+    rep : int
+        Representation index
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    int
+        Minimum dimension of any associated decay operator
+    """
     ind = abs(rep) - 1
     d_min = repinfo[ind][3]
     return d_min
 
 @njit
 def encalc_times_36(reps: np.ndarray[int], repinfo: np.ndarray = repinfo) -> tuple[int, int]:
+    """
+    Compute the anomaly coefficients E and N for a given set of representations
+
+    Parameters
+    ----------
+    reps : np.ndarray[int]
+        Array of representation indices
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    tuple[int, int]
+        Anomaly coefficients \f$36E\f$ and \f$36N\f$
+
+    Notes
+    -----
+    - The anomaly coefficients are multiplied by 36 to represent them as integers
+    """
     e, n = 0, 0
     for rep in reps:
         r3, r2, r1, ind, sgn = charges_from_rep(rep, repinfo)
@@ -54,6 +149,21 @@ def encalc_times_36(reps: np.ndarray[int], repinfo: np.ndarray = repinfo) -> tup
 
 @njit
 def running_Q_contrib(model: np.ndarray[int], repinfo: np.ndarray = repinfo) -> tuple[np.ndarray[float], ...]:
+    """
+    Compute the the contributions from the heavy quarks (\f$\mathcal{Q}\f$s) to the running of the gauge couplings
+
+    Parameters
+    ----------
+    model : np.ndarray[int]
+        Array of representation indices of the heavy quarks
+    repinfo : np.ndarray
+        Array of representation information (default: repinfo from file)
+
+    Returns
+    -------
+    tuple[np.ndarray[float], np.ndarray[float]]
+        Tuple of the coefficients \f$a_{\mathcal{Q}}\f$ and \f$b_{\mathcal{Q}}\f$ for the heavy quarks
+    """
     a_bSM = np.zeros(3)
     b_bSM = np.zeros((3,3))
     kappa = 1
@@ -81,6 +191,31 @@ def running_Q_contrib(model: np.ndarray[int], repinfo: np.ndarray = repinfo) -> 
 
 @njit
 def running(t, y, a_SM, b_SM, a_bSM, b_bSM, mQ):
+    """
+    Equation for the running of the gauge couplings at 2-loop order
+
+    Parameters
+    ----------
+    t : float
+        RG integration variable
+    y : np.ndarray[float]
+        Array of the inverse gauge couplings
+    a_SM : np.ndarray[float]
+        Array of 1-loop coefficients for the SM contributions
+    b_SM : np.ndarray[float]
+        Array of 2-loop coefficients for the SM contributions
+    a_bSM : np.ndarray[float]
+        Array of 1-loop coefficients for the heavy quark contributions
+    b_bSM : np.ndarray[float]
+        Array of 2-loop coefficients for the heavy quark contributions
+    mQ : float
+        Mass of the heavy quarks in GeV
+
+    Returns
+    -------
+    np.ndarray[float]
+        Array of the derivatives of the inverse gauge couplings
+    """
     tQ = np.log(mQ/MASS_Z)/(2*np.pi)
     a = a_SM + (t > tQ)*a_bSM
     b = b_SM + (t > tQ)*b_bSM
@@ -90,6 +225,7 @@ def running(t, y, a_SM, b_SM, a_bSM, b_bSM, mQ):
     return dydt
 
 """
+@TODO Can potentially use the Jacobian for the solver
 @njit
 def jac(t, y, unused1, b_SM, unused2, b_bSM, mQ):
     tQ = np.log(mQ/MASS_Z)/(2*np.pi)
@@ -100,6 +236,19 @@ def jac(t, y, unused1, b_SM, unused2, b_bSM, mQ):
 
 @njit
 def hit_LP(unused1: float, y: np.ndarray[float], *unused2: tuple[any, ...]) -> float:
+    """
+    Event function to stop the integration when the Landau pole is hit
+
+    Returns
+    -------
+    float
+        Minimum value of the inverse gauge couplings (zero when the Landau pole is hit)
+
+    Notes
+    -----
+    - The parameters unused1 and unused2 are only there to match the signature of the event function
+    - The event is terminal
+    """
     return min(y)
 hit_LP.direction = -1
 hit_LP.terminal = True
@@ -113,16 +262,42 @@ b_SM = (-b1+b2+b3).T
 
 @njit
 def param_conversion(t: float) -> float:
+    """
+    Parameter conversion from the RG time to the energy scale \f$E = M_Z\exp(2\pi\mathrm{t})\f$
+
+    Parameters
+    ----------
+    t : float
+        RG variable
+
+    Returns
+    -------
+    float
+        Energy scale in GeV
+    """
     return MASS_Z*np.exp(2*np.pi*t)
 
 @njit
 def inv_param_conversion(erg: float) -> float:
+    """
+    Inverse parameter conversion from the energy scale to the RG time \f$\mathrm{t} = \log(E/M_Z)/(2\pi)\f$
+
+    Parameters
+    ----------
+    erg : float
+        Energy scale in GeV
+
+    Returns
+    -------
+    float
+        RG variable
+    """
     return np.log(erg/MASS_Z)/(2*np.pi)
 
 def find_LP(model: list[int], mQ: float = 5e11, lp_threshold: float = 1e18, verbose: bool = True, plot: bool = False) -> tuple[float, int]:
     """
     Find the (lowest) Landau pole (LP) in the running of the gauge couplings.
-    
+
     Parameters
     ----------
     model : list[int]
@@ -135,12 +310,12 @@ def find_LP(model: list[int], mQ: float = 5e11, lp_threshold: float = 1e18, verb
         Whether to print additional information or not (default: True)
     plot : bool
         Whether to plot the running of the gauge couplings or not (default: False)
-    
+
     Returns
     -------
     tuple[float, int]
         A tuple of the mass scale of the LP in GeV and the index of the LP in the array of gauge couplings
-        
+
     Notes
     -----
     - The running of the gauge couplings is solved using a (4,5)-Runge-Kutta method with adaptive step size control
